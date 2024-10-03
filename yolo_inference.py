5552c9fb9f499a6e28e81913d6e27786f97f4244
jaykold <christianichebi@gmail.com>
2024-10-03 02:27:47 +0100

    updated yolo_inference & .gitignore

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,3 +1,8 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8')  # Load model
+
+results = model.predict(save=True, stream=True) # Inference on video stream
+
+for box in results[0].boxes:
+    print(box)  # print class and box coordinates

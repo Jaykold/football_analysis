156ebc8f372a0c8f91db4982dfb3f52cc95e0f50
jaykold <christianichebi@gmail.com>
2024-10-17 22:41:24 +0100

    updated main & tracker file

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def main():
     video_input_path = 'input_videos/08fd33_4.mp4'
     video_output_path = 'output_videos/output_video.avi'
     model_path = 'models/best.pt'
+    stub_path = 'stubs/track_stubs.pkl'
 
     # Read video
     video_frames = read_video(video_input_path)
 
     tracker = Tracker(model_path)
 
-    tracker.get_object_tracks(video_frames)
+    tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path=stub_path)
 
     # Save video
     save_video(video_frames, video_output_path)
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,5 +1,7 @@
+import os
 from ultralytics import YOLO
 import supervision as sv
+import pickle
 
 class Tracker:
     def __init__(self, model_path):
@@ -13,11 +15,21 @@ class Tracker:
             batch = frames[i:i+batch_size]
             detections_batch = self.model.predict(batch, conf=0.1)
             detections.extend(detections_batch)
-            break
         return detections
     
-    def get_object_tracks(self, frames):
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as f:
+                return pickle.load(f)
+            
         detections = self.detect_frames(frames)
+
+        tracks = {
+            "players": [], # {0:{"box": [0,0,0,0]}, 1:{"box": [0,1,0,2]}, 5:{"box": [0,0,0,0]}}
+            "referees": [],
+            "ball": []
+        }
 
         for frame_num, detection in enumerate(detections):
             cls_names = detection.names
@@ -33,5 +45,32 @@ class Tracker:
             
             # Track objects
             detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+
+            tracks["players"].append({})
+            tracks["referees"].append({})
+            tracks["ball"].append({})
+
+            # Store tracks
+            for track in detection_with_tracks:
+                bbox = track[0].tolist()
+                cls_id = track[3]
+                track_id = track[4]
+
+                if cls_id == cls_names_inv["player"]:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+                
+                if cls_id == cls_names_inv["referee"]:
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+
+            for track in detection_supervision:
+                bbox = track[0].tolist()
+                cls_id = track[3]
+
+                if cls_id == cls_names_inv["ball"]:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+
+            if stub_path is not None:
+                with open(stub_path, 'wb') as f:
+                    pickle.dump(tracks, f)
             
             print(detection_with_tracks)
